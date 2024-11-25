6569ba95f5dbd3aa1197ad52f5c38dd99a8a52b7
Kanisorn-S <karamahati@gmail.com>
2024-11-25 15:31:51 +0700

    feat: move dht22 to raspberry pi

--- RaspberryPi/src/utils/dht.py
+++ RaspberryPi/src/utils/dht.py
@@ -1,67 +1,21 @@
-import RPi.GPIO as GPIO
-import time
+import time 
+import board 
+import adafruit_dht
 
-DHT_PIN = 4  # GPIO pin where the DHT22 is connected
+sensor = adafruit_dht.DHT22(board.D4)
 
-def read_dht22():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(DHT_PIN, GPIO.OUT)
-    GPIO.output(DHT_PIN, GPIO.LOW)
-    time.sleep(0.02)
-    GPIO.output(DHT_PIN, GPIO.HIGH)
-    GPIO.setup(DHT_PIN, GPIO.IN)
-
-    data = []
-    for i in range(500):
-        data.append(GPIO.input(DHT_PIN))
-
-    bit_count = 0
-    tmp = 0
-    count = 0
-    humidity_bit = ""
-    temperature_bit = ""
-    crc = ""
-
+while True:
     try:
-        while data[count] == 1:
-            count += 1
+        tempC = sensor.temperature
+        hum = sensor.humidity
+        print("Temp: ", tempC)
+        print("Hum: ", hum)
+    except RuntimeError as error:
+        print(error.args[0])
+        time.sleep(2)
+        continue
+    except Exception as errror:
+        sensor.exit()
+        raise error
 
-        for i in range(32):
-            while data[count] == 0:
-                count += 1
-            while data[count] == 1:
-                tmp += 1
-                count += 1
-            if tmp > 3:
-                if bit_count < 16:
-                    humidity_bit += "1"
-                if bit_count >= 16 and bit_count < 32:
-                    temperature_bit += "1"
-            else:
-                if bit_count < 16:
-                    humidity_bit += "0"
-                if bit_count >= 16 and bit_count < 32:
-                    temperature_bit += "0"
-            bit_count += 1
-            tmp = 0
-
-        for i in range(8):
-            while data[count] == 0:
-                count += 1
-            while data[count] == 1:
-                tmp += 1
-                count += 1
-            if tmp > 3:
-                crc += "1"
-            else:
-                crc += "0"
-            tmp = 0
-
-        humidity = int(humidity_bit, 2) / 10.0
-        temperature = int(temperature_bit, 2) / 10.0
-
-        return {'temperature': temperature, 'humidity': humidity}
-    except:
-        return {'error': 'Failed to retrieve data from humidity sensor'}
-    finally:
-        GPIO.cleanup()
+    time.sleep(3)
