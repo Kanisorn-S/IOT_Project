4121f7336d55d6ec2b0db2bb1c7278626ce2c54b
Kanisorn-S <karamahati@gmail.com>
2024-11-30 11:35:40 +0700

    feat: update messaging api

--- messaging-api/response_message.py
+++ messaging-api/response_message.py
@@ -20,12 +20,26 @@ def response_message(event, msg):
       "status": "status",
       "picture": "pic",
    }
+   status_convert = {
+      "2": "High risk of Spoilage",
+      "1": "Risk of Rotten. Improper Storing Condition",
+      "0": "Fresh",
+   }
    requested_attr = convert_word[requested]
    if requested_attr == "color":
       response = "The current color is R: " + str(sensors_data["data"]["red"]) + " G: " + str(sensors_data["data"]["green"]) + " B: " + str(sensors_data["data"]["blue"])
    elif requested_attr == 'pic':
       return 1
-   elif sensors_data["data"][str(requested_attr)]:
+   elif requested_attr == 'status':
+      response = "The current status is " + str(status_convert[str(sensors_data["data"]["status"])])
+   elif sensors_data["data"][str(requested_attr)] is not None:
       response = "The current " + str(requested) + " is " + str(sensors_data["data"][str(requested_attr)])
+      if requested_attr == "temp":
+         response += "°C"
+      elif requested_attr == "hum":
+         response += "%"
+      elif requested_attr == "alc":
+         response += "% Change"
+      
 
    return TextMessage(text=response)
